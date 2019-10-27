fe619ddbf713607e9d759eb710f5c65880a74a1f
AMK <amk9978@gmail.com>
2019-10-27 14:30:32 +0330

    Socket is running

---
+++ app/src/main/java/com/amk/morris/Adapters/ChatBoxAdapter.java
@@ -0,0 +1,55 @@
+package com.amk.morris.Adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.amk.morris.Model.Message;
+import com.amk.morris.R;
+
+import java.util.List;
+
+
+public class ChatBoxAdapter  extends RecyclerView.Adapter<ChatBoxAdapter.MyViewHolder> {
+    private List<Message> MessageList;
+
+    public  class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView nickname;
+        public TextView message;
+
+        public MyViewHolder(View view) {
+            super(view);
+            nickname = view.findViewById(R.id.nickname);
+            message = view.findViewById(R.id.message);
+        }
+    }
+    public ChatBoxAdapter(List<Message>MessagesList) {
+        this.MessageList = MessagesList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return MessageList.size();
+    }
+    @Override
+    public ChatBoxAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item, parent, false);
+
+        return new ChatBoxAdapter.MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final ChatBoxAdapter.MyViewHolder holder, final int position) {
+        final Message m = MessageList.get(position);
+        holder.nickname.setText(m.getNickname() +" : ");
+
+        holder.message.setText(m.getMessage() );
+
+    }
+
+}
--- app/src/main/java/com/amk/morris/SocketAct.java
+++ app/src/main/java/com/amk/morris/SocketAct.java
@@ -2,60 +2,129 @@ package com.amk.morris;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.amk.morris.Adapters.ChatBoxAdapter;
+import com.amk.morris.Model.Message;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URISyntaxException;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SocketAct extends AppCompatActivity {
-
+    public RecyclerView myRecylerView;
+    public List<Message> MessageList;
+    public ChatBoxAdapter chatBoxAdapter;
+    public EditText messagetxt;
+    public Button send;
+    //declare socket object
     private Socket socket;
+    private String OP_ID = "";
+    public String Nickname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_socket);
+
+        messagetxt = (EditText) findViewById(R.id.message);
+        send = (Button) findViewById(R.id.send);
+        // get the nickame of the user
+        Nickname = "AMK";
+        //connect you socket client to the server
         try {
             socket = IO.socket("http://morrisfa.eu-4.evennode.com:80");
             socket.connect();
-            socket.emit("join", "Haji");
-            if (socket.connected())
-                Log.i("TAG", "CONNECTED");
-            else
-                Log.i("TAG", "NOT");
+            socket.emit("join", Nickname);
         } catch (URISyntaxException e) {
-            Log.i("TAG", Objects.requireNonNull(e.getMessage()));
             e.printStackTrace();
+
         }
+        //setting up recyler
+        MessageList = new ArrayList<>();
+        myRecylerView = findViewById(R.id.messagelist);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        myRecylerView.setLayoutManager(mLayoutManager);
+        myRecylerView.setItemAnimator(new DefaultItemAnimator());
+
+
+        // message send action
+        send.setOnClickListener(v -> {
+            //retrieve the nickname and the message content and fire the event messagedetection
+            if (!messagetxt.getText().toString().isEmpty()) {
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("msg", messagetxt.getText().toString());
+                    jsonObject.put("opid", OP_ID);
+                    socket.emit("movement", jsonObject.toString());
+                    Log.i("TAG", jsonObject.toString());
+                    Log.i("TAG", "MINE");
+                    messagetxt.setText(" ");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         //implementing socket listeners
         socket.on("connection", args -> runOnUiThread(() -> {
             String data = (String) args[0];
-
-            Toast.makeText(SocketAct.this, data, Toast.LENGTH_SHORT).show();
-
+            Toast.makeText(SocketAct.this, data, Toast.LENGTH_LONG).show();
+            Log.i("TAG", "Connection:" + data);
         }));
         socket.on("disconnect", args -> runOnUiThread(() -> {
             String data = (String) args[0];
-            Toast.makeText(SocketAct.this, data, Toast.LENGTH_SHORT).show();
+            Log.i("TAG", "DISConnection:" + data);
+            Toast.makeText(SocketAct.this, data, Toast.LENGTH_LONG).show();
 
         }));
         socket.on("message", args -> runOnUiThread(() -> {
             JSONObject data = (JSONObject) args[0];
+            Log.i("TAG", "MESSAGE:" + data.toString());
+        }));
+        socket.on("winner", args -> runOnUiThread(() -> {
+            JSONObject data = (JSONObject) args[0];
+            Log.i("TAG", "WINNER:" + data.toString());
+        }));
+        socket.on("movement", args -> runOnUiThread(() -> {
+            String data = (String) args[0];
+            Log.i("TAG", "MOVEMENT:" + data);
             try {
-                //extract data from fired event
-                Log.i("TAG", data.toString());
-            } catch (Exception e) {
+                JSONObject jsonObject = new JSONObject(data);
+                Toast.makeText(SocketAct.this, jsonObject.getString("msg"), Toast.LENGTH_SHORT).show();
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
 
-
         }));
+        socket.on("start", args -> runOnUiThread(() -> {
+            JSONObject data = (JSONObject) args[0];
+            Log.i("TAG", "START:" + data.toString());
+            try {
+                OP_ID = data.getString("opid");
+                Log.i("TAG", OP_ID);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }));
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        socket.disconnect();
     }
 }
